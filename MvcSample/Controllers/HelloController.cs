1a69d03e17a1d23c7ad3557999551ff201731038
wphr2101 <wphr2101@hotmail.com>
2017-07-29 00:25:42 -0400

    First Commit

---
+++ MvcSample/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSample.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            return View("Index","", "You");
+        }
+    }
+}
---
+++ MvcSample/Models/Country.cs
@@ -0,0 +1,13 @@
+﻿namespace MvcSample.Models
+{
+    public class Country
+    {
+        public int id;
+        public string name;
+        public string capital;
+        public string description;
+        public int area;
+        public int population;
+        public int gdp;
+    }
+}
